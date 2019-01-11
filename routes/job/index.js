ef4bf54e989f24c8ed9734d8bd1861c8b38fb0f2
growable <yhdeng86@qq.com>
2019-01-11 22:30:18 +0800

    添加Job请求入口功能

--- routes/job/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var ipRoutes = require('./ip');
-
-router.all('/ip/', ipRoutes);
-
-module.exports = router;
--- routes/job/ip.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var ipController = require('../../job/controllers/ip')
-
-router.route('/job/ip/search').post('');
-
-module.exports = router;
