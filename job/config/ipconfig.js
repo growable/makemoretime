f321fdb007e105ecdb1396dcb29c66a05c38d6b2
growable <yhdeng86@qq.com>
2019-01-12 17:01:22 +0800

    获取ip Html数据

--- job/config/ipconfig.js
+++ job/config/ipconfig.js
@@ -1,9 +1,9 @@
-module.exports = {
-  xici: {
-    url: '',
-    type: [],
-    pageName: '',
-    pages: 10, // 需要获取页数
-    regExp: '' // 正则获取ip, port，city 字段
+module.exports = [
+  {
+    name: 'xici',
+    url: 'https://www.xicidaili.com/{type}/{pageNo}',
+    type: ['nn'],
+    pages: 5, // 需要获取页数
+    regExp: '<tr class=".*?">[\s\S]*?<\/tr>' // 正则获取ip, port，city 字段
   }
-};
+];
--- job/controllers/ip.js
+++ job/controllers/ip.js
@@ -1,4 +1,5 @@
-
+var async = require('async');
+var crawler = require('crawler');
 var ipConfig = require('../config/ipconfig');
 
 /**
@@ -7,7 +8,32 @@ var ipConfig = require('../config/ipconfig');
  * @param {*} callback
  */
 exports.get = function (params = {}, callback) {
-  callback(null, params);
+  if (ipConfig.length > 0) {
+    async.mapLimit(ipConfig, 10, function (config, cb) {
+      for (const type of config.type) {
+        for (let pageNo = 1; pageNo <= config.pages; pageNo++) {
+          const url = config.url.replace('{type}', type).replace('{pageNo}', pageNo);
+          async.waterfall([
+            // get page content
+            function (cb) {
+              getPageContent(url, cb);
+            },
+            // filter page content
+            function (pageContent, cb) {
+              filterPageContent(pageContent, config.regExp, cb);
+            }
+          ], function (err, result) {
+            // cb(err, result);
+            console.log(result);
+          });
+        }
+      }
+    }, function (err, result) {
+      callback(err, result);
+    });
+  } else {
+    callback(null, 'ip config is null');
+  }
 };
 
 /**
@@ -18,3 +44,28 @@ exports.get = function (params = {}, callback) {
 exports.check = function (params = {}, callback) {
 
 };
+
+/**
+ * get page content
+ * @param {*} url
+ * @param {*} callback
+ */
+function getPageContent (url = '', callback) {
+  var c = new crawler({
+    callback: function (error, res, done) {
+      callback(error, res);
+      done();
+    }
+  });
+  c.queue(url);
+}
+
+/**
+ * filter page content with regExp
+ * @param {*} pageContent
+ * @param {*} regExp
+ * @param {*} callback
+ */
+function filterPageContent (pageContent = '', regExp = '', callback) {
+  callback(null, pageContent);
+}
