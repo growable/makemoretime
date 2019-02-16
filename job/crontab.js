136ace2944308c0a83c5cf5a3ecbfd96e5a1fdf4
growable <yhdeng86@qq.com>
2019-02-16 12:57:05 +0800

    修复cron没有回调函数导致的错误

--- job/crontab.js
+++ job/crontab.js
@@ -4,16 +4,22 @@ const ip = require('./controllers/ip');
 const lianjia = require('./controllers/lianjia');
 
 //每5分钟获取一次ip
-new cronJob('0 */5 * * * *', function () {
-  ip.get();
+new cronJob('0 */1 * * * *', function () {
+  ip.get({}, function (err, result) {
+    console.log(err)
+  });
 }, null, true, 'Asia/Chongqing');
 
 //每5分钟验证一次ip
 new cronJob('0 */5 * * * *', function () {
-  ip.check();
+  ip.check({}, function (err, result) {
+    console.log(err)
+  });
 }, null, true, 'Asia/Chongqing');
 
 //每天执行一次获取二手房价格数据
 new cronJob('1 0 * * * *', function () {
-  lianjia.house();
+  lianjia.house({}, function (err, result) {
+    console.log(err)
+  });
 }, null, true, 'Asia/Chongqing');
